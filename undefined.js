289121c83f7098d1176edeeecba24385436c6122
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-08 17:21:35 -0300

    .
    .

---
+++ undefined.js
@@ -0,0 +1,22 @@
+// undefined
+
+let produto = `Camisa Polo`;
+let carrinho = undefined;
+let numero = 50;
+let preco = 50.0;
+let ligado = true;
+let desligado = false;
+
+console.log(typeof produto); // string
+console.log(typeof carrinho); // undefined
+console.log(typeof numero); // number
+console.log(typeof preco); // number
+console.log(typeof ligado); // boolean // true
+console.log(typeof desligado); // boolean // false
+
+// Null
+
+let lista = null;
+
+console.log(lista); // null
+console.log(typeof lista); // object
