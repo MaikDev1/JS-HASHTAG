1cd06037e25e8a2af0cff011538ba46d43ed2de8
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-08 16:59:38 -0300

    .
    .

---
+++ boolean.js
@@ -0,0 +1,15 @@
+// boolean
+
+let ligado = true; // true
+let desligado = false; // false
+
+console.log(ligado); // true
+console.log(desligado); // false
+
+let name = "Maik";
+let sobreNome = "Alves da Silva";
+let idade = 27;
+
+console.log(Boolean(name)); // true
+console.log(Boolean(sobreNome)); // true
+console.log(Boolean(idade)); // true
