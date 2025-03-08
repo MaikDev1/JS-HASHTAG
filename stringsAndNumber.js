a2c9fbc399a5e73773153613ccbe2aefc45d8304
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-08 11:20:13 -0300

    .
    .

---
+++ stringsAndNumber.js
@@ -0,0 +1,12 @@
+// String
+
+// "um texto"
+// 'Cria uma mensagem'
+// `Cria mais um texto` - string template
+
+// Concatenação
+let nome = "Lucas";
+let mensagem = `Seja bem vindo`;
+let mensagem2 = `Olá, ${nome} ${mensagem} `; // Seja bem vindo, Lucas
+
+console.log(mensagem2);
