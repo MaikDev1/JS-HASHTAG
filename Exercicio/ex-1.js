f62fc045fe7e384cb01d738d36573ca456f78bc6
Maik Alves da Silva <125711894+MaikDev1@users.noreply.github.com>
2025-03-08 17:28:30 -0300

    .
    .

---
+++ Exercicio/ex-1.js
@@ -0,0 +1,8 @@
+// Exercicio 1
+
+// Exercício 1: Escreva um programa simples que exiba uma string no terminal utilizando
+
+console.log("Olá, mundo!"); // Olá, mundo!
+console.log("Tudo bem com você? "); // Tudo bem com você?
+console.log("Seja bem vindo, Maik"); // Seja bem vindo, Maik
+console.log("Olá, Maik, tudo bem com voce ?"); // Olá, Maik, tudo bem com voce
